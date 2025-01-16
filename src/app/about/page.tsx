7ea2a926fc5d52483fb624a79af3c1ab68408f22
arpitsahuu <arpitsahu358@gmail.com>
2025-01-17 02:49:36 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import { Metadata } from 'next'
+import Story from '@/components/about/Story'
+import Mission from '@/components/about/Mission'
+import GetInTouch from '@/components/about/GetInTouch'
+
+export const metadata: Metadata = {
+  title: 'About Us | My Blog',
+  description: 'Learn more about our blog, our mission, and the team behind it.',
+}
+
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold text-center text-gray-900 mb-8">About Us</h1>
+      <Story/>
+      <Mission/>
+      <GetInTouch/>
+    </div>
+  )
+}
---
+++ src/lib/posts.ts
@@ -0,0 +1,53 @@
+import { cache } from 'react'
+import axios from 'axios';
+
+
+
+
+// Fetch posts with pagination
+export const fetchPosts = async (page: number = 1, limit: number = 5) => {
+  try {
+    // Construct the URL with query parameters for page and limit
+    const url = `http://localhost:3050/api/v1/blogs?page=${page}&limit=${limit}`;
+    
+    const response = await axios.get(url); // Fetch posts from the backend
+    console.log(response.data.blogs)
+    return response.data; // Assuming the server returns a paginated response
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
+};
+
+// Get posts with pagination
+export const getPosts = cache(async (page: number = 1, postsPerPage: number = 5) => {
+  const data = await fetchPosts(page, postsPerPage); // Fetch posts with pagination
+
+  return {
+    posts: data?.posts, // Assuming the server returns the posts in a `posts` field
+    totalPages: data?.totalPages, // Assuming the server provides the total page count
+  };
+});
+
+
+export const getPostById = cache(async (id: string) => {
+  try {
+    const url = `http://localhost:3050/api/v1/blog/${id}`; // Assuming the backend has a route to fetch a post by ID
+    const response = await axios.get(url);
+    return response?.data; // Assuming the backend returns the post directly
+  } catch (error) {
+    console.error('Error fetching post by ID:', error);
+  }
+});
+
+export const searchPosts = cache(async (query: string) => {
+  try {
+    // Send the search query to the server
+    const url = `http://localhost:3050/api/v1/blogs?search=${encodeURIComponent(query)}&limit=5`; // Limit to 5 results
+    const response = await axios.get(url);
+
+    return response?.data?.posts; // Assuming the server returns posts in the `posts` field
+  } catch (error) {
+    console.error('Error fetching search results:', error);
+  }
+});
+
